208af16e87dbeb053c43099141c43c2699ddfb28
nguyenkhoa <khoanguyen109.dev@gmail.com>
2023-07-10 21:40:48 +0700

    code

---
+++ Bai2/Bai2/Model/ObjType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+
+namespace Bai2.Model
+{
+    public class ObjType
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Shape_type { get; set; }
+
+    }
+
+
+
+
+}
---
+++ Bai2/Bai2/Model/TagImage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Drawing;
+using TagLib;
+
+namespace Bai2
+{
+    public class TagImage : TagPicture
+    {
+        public string NameFolder { get; set;}
+       
+        public override IEnumerable<object> GetDataSourceForDataGridView()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public List<TagObject> ListTagObjects = new List<TagObject>();
+
+        //public void Draw(Graphics g)
+        //{
+        //    foreach (var item in ListTagObjects)
+        //    {
+        //        item.Draw(g);
+        //    }
+        //}
+
+    }
+}
